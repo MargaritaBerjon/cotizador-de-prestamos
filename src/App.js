3216a90eb176801b76a4dfe37e6561fcc0a7ce1e
Margui <margui.berjon@gmail.com>
2020-09-02 13:21:46 +0200

    feature: create form component

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Header from './components/Header.js'
 import Form from './components/Form.js'
 
@@ -6,11 +6,25 @@ import Form from './components/Form.js'
 import './App.css';
 
 function App() {
+  const [quantity, setQuantity] = useState(0);
+  const [term, setTerm] = useState(0);
+  const [total, setTotal] = useState(0);
+
+
+
   return (
     <>
       <Header title='Cotizador de prestamos'></Header>
       <div className='container'>
-        <Form></Form>
+        <Form
+          quantity={quantity}
+          setQuantity={setQuantity}
+          term={term}
+          setTerm={setTerm}
+          total={total}
+          setTotal={setTotal}>
+        </Form>
+        <p>Total a pagar: {total}€</p>
       </div>
     </>
   );
--- src/components/Form.js
+++ src/components/Form.js
@@ -1,40 +1,71 @@
 import React, { useState } from 'react';
+import { calculateTotal } from '../helpers.js'
 
-const Form = () => {
-  const [quantity, setQuantity] = useState(0);
+
+const Form = (props) => {
+  const { quantity, setQuantity, term, setTerm, total, setTotal } = props;
+  const [error, setError] = useState(false)
+
+
+  const readQueantity = (e) => {
+    setQuantity(parseInt(e.target.value))
+  }
+
+  const readTerm = (e) => {
+    setTerm(parseInt(e.target.value))
+  }
+
+  const calculateAmount = (e) => {
+    e.preventDefault();
+    if (quantity === 0 || term === 0) {
+      setError(true);
+      return;
+    }
+    setError(false);
+    const totalAmount = calculateTotal(quantity, term);
+
+    setTotal(totalAmount);
+
+  }
 
   return (
-    <form>
-      <div className="row">
-        <div>
-          <label>Cantidad Prestamo</label>
-          <input
-            className="u-full-width"
-            type="number"
-            placeholder="Ejemplo: 3000"
-          />
+    <>
+      <form onSubmit={calculateAmount}>
+        <div className="row">
+          <div>
+            <label>Cantidad Prestamo</label>
+            <input
+              className="u-full-width"
+              type="number"
+              placeholder="Ejemplo: 3000"
+              onChange={readQueantity}
+            />
+          </div>
+          <div>
+            <label>Plazo para Pagar</label>
+            <select
+              className="u-full-width"
+              onChange={readTerm}
+            >
+              <option value="">Seleccionar</option>
+              <option value="3">3 meses</option>
+              <option value="6">6 meses</option>
+              <option value="12">12 meses</option>
+              <option value="24">24 meses</option>
+            </select>
+          </div>
+          <div>
+            <input
+              type="submit"
+              value="Calcular"
+              className="button-primary u-full-width"
+            />
+          </div>
         </div>
-        <div>
-          <label>Plazo para Pagar</label>
-          <select
-            className="u-full-width"
-          >
-            <option value="">Seleccionar</option>
-            <option value="3">3 meses</option>
-            <option value="6">6 meses</option>
-            <option value="12">12 meses</option>
-            <option value="24">24 meses</option>
-          </select>
-        </div>
-        <div>
-          <input
-            type="submit"
-            value="Calcular"
-            className="button-primary u-full-width"
-          />
-        </div>
-      </div>
-    </form>
+      </form>
+      {error ? <p className='error'>Todos los campor son obligatorios</p> : null}
+
+    </>
   );
 }
 
